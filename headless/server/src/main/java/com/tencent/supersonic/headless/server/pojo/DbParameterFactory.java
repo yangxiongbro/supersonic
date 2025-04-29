99c1bdb508a212b81d0501576cbd1b3b12a2d121
yangxiongbro <yangxiongbro@163.com>
2025-04-29 10:58:32 +0800

    fix:数据库管理增加oracle支持

--- headless/server/src/main/java/com/tencent/supersonic/headless/server/pojo/DbParameterFactory.java
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/pojo/DbParameterFactory.java
@@ -20,6 +20,8 @@ public class DbParameterFactory {
         parametersBuilder.put(EngineType.KYUUBI.getName(), new KyuubiParametersBuilder());
         parametersBuilder.put(EngineType.PRESTO.getName(), new PrestoParametersBuilder());
         parametersBuilder.put(EngineType.TRINO.getName(), new TrinoParametersBuilder());
+        parametersBuilder.put(EngineType.TRINO.getName(), new TrinoParametersBuilder());
+        parametersBuilder.put(EngineType.ORACLE.getName(), new OracleParametersBuilder());
         parametersBuilder.put(EngineType.OTHER.getName(), new OtherParametersBuilder());
     }
 
