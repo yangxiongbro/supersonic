e156e3aa90e57d9a0bf2df57cf1a755851a4a0b8
yangxiongbro <yangxiongbro@163.com>
2025-05-12 11:51:16 +0800

    fix:适配ORACLE limit

--- headless/core/src/main/java/com/tencent/supersonic/headless/core/translator/optimizer/ResultLimitOptimizer.java
+++ headless/core/src/main/java/com/tencent/supersonic/headless/core/translator/optimizer/ResultLimitOptimizer.java
@@ -1,6 +1,7 @@
 package com.tencent.supersonic.headless.core.translator.optimizer;
 
 import com.tencent.supersonic.common.jsqlparser.SqlSelectHelper;
+import com.tencent.supersonic.common.pojo.enums.EngineType;
 import com.tencent.supersonic.headless.core.pojo.QueryStatement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,10 @@ public class ResultLimitOptimizer implements QueryOptimizer {
 
     @Override
     public void rewrite(QueryStatement queryStatement) {
-        queryStatement.setSql(queryStatement.getSql() + " LIMIT " + queryStatement.getLimit());
+        if(EngineType.ORACLE.equals(EngineType.fromString(queryStatement.getOntology().getDatabase().getType()))){
+            queryStatement.setSql(queryStatement.getSql() + " WHERE ROWNUM <=" + queryStatement.getLimit());
+        } else {
+            queryStatement.setSql(queryStatement.getSql() + " LIMIT " + queryStatement.getLimit());
+        }
     }
 }
